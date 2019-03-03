8d64457d72340b75cee5a2b0242cb9e8e03ce0df
hhe1667 <42723533+hhe1667@users.noreply.github.com>
2019-03-03 03:31:01 -0800

    Use LeNet model. Driving much better.

--- model.py
+++ model.py
@@ -1,23 +1,22 @@
 import math
-import random
 import os.path
+import random
 
 import cv2
 import numpy as np
 import pandas as pd
-from absl import app
-from absl import flags
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers import Convolution2D
 import sklearn.model_selection as skms
 import sklearn.utils as sku
+from absl import app
+from absl import flags
+from keras import layers
+from keras.models import Sequential
 
 flags.DEFINE_string("csv", "~/work/data/behavioral-cloning/driving_log.csv",
                     "Driving log CSV file.")
 flags.DEFINE_string("root", "~/work/data/behavioral-cloning/IMG/",
                     "Root directory for images.")
-flags.DEFINE_integer("epochs", 5, "Number of epochs.")
+flags.DEFINE_integer("epochs", 10, "Number of epochs.")
 FLAGS = flags.FLAGS
 
 
@@ -29,10 +28,6 @@ def read_driving_log(csv_path, root):
     df[col] = df[col].apply(
       lambda x: os.path.join(root, os.path.basename(x)))
 
-  images = [cv2.imread(img_path) for img_path in df["center_img"]]
-  images = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB) for img in images]
-  print("#images: %d" % len(images))
-  angles = df["steering_angle"]
   return df
 
 
@@ -44,6 +39,7 @@ def images_generator(img_df, batch_size=32):
     for offset in range(0, num, batch_size):
       batch = img_df[offset:offset + batch_size]
       images = [cv2.imread(img_path) for img_path in batch["center_img"]]
+      images = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB) for img in images]
       angles = batch["steering_angle"]
       X_train = np.array(images)
       y_train = np.array(angles)
@@ -59,10 +55,35 @@ def driving_model(img_df, batch_size=32):
   valid_steps = math.ceil(len(valid_df) / batch_size)
 
   model = Sequential()
-  model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-  
-  model.add(Flatten(input_shape=(160, 320, 3)))
-  model.add(Dense(1))
+  model.add(layers.Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+
+  # Conv1, output shape: (156, 316, 6)
+  model.add(layers.Conv2D(filters=6, kernel_size=5, strides=1,
+                          activation="relu", input_shape=(160, 320, 3)))
+  # Output shape: (78, 158, 6)
+  model.add(layers.MaxPooling2D(pool_size=2, strides=2))
+  # dropout?
+
+  # Conv2, output shape: (74, 154, 16)
+  model.add(
+    layers.Conv2D(filters=16, kernel_size=5, strides=1, activation="relu",
+                  input_shape=(78, 158, 6)))
+  # Output shape: (37, 77, 16)
+  model.add(layers.MaxPooling2D(pool_size=2, strides=2))
+  # Dropout?
+
+  # Flatten
+  model.add(layers.Flatten())
+
+  # FC1:
+  model.add(layers.Dense(units=120, activation="relu"))
+  # Dropout?
+
+  # FC2:
+  model.add(layers.Dense(units=84, activation="relu"))
+  # Dropout?
+
+  model.add(layers.Dense(1))
 
   model.compile(loss="mse", optimizer="adam")
 
