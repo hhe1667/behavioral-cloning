0fccbafa6cd4cf72916fd293596de24c5d84a60d
hhe1667 <42723533+hhe1667@users.noreply.github.com>
2019-03-03 14:19:32 -0800

    Augment images by flipping.

--- model.py
+++ model.py
@@ -17,6 +17,7 @@ flags.DEFINE_string("csv", "~/work/data/behavioral-cloning/driving_log.csv",
 flags.DEFINE_string("root", "~/work/data/behavioral-cloning/IMG/",
                     "Root directory for images.")
 flags.DEFINE_integer("epochs", 10, "Number of epochs.")
+flags.DEFINE_integer("batch_size", 32, "Batch size.")
 FLAGS = flags.FLAGS
 
 
@@ -31,27 +32,40 @@ def read_driving_log(csv_path, root):
   return df
 
 
-def images_generator(img_df, batch_size=32):
+def images_generator(img_df, training=True, batch_size=32):
   num = len(img_df)
   random.seed(1)
   while True:
     img_df = sku.shuffle(img_df)
-    for offset in range(0, num, batch_size):
-      batch = img_df[offset:offset + batch_size]
-      images = [cv2.imread(img_path) for img_path in batch["center_img"]]
-      images = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB) for img in images]
-      angles = batch["steering_angle"]
-      X_train = np.array(images)
-      y_train = np.array(angles)
-      yield X_train, y_train
+    images = []
+    angles = []
+    for _, row in img_df.iterrows():
+      img = cv2.imread(row["center_img"])
+      img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+      angle = row["steering_angle"]
+      images.append(img)
+      angles.append(angle)
+
+      # Flipping image
+      if training:
+        images.append(np.fliplr(img))
+        angles.append(-angle)
+      if len(images) >= batch_size:
+        X_train = np.array(images[:batch_size])
+        y_train = np.array(angles[:batch_size])
+        del images[:batch_size]
+        del angles[:batch_size]
+        yield X_train, y_train
 
 
 def driving_model(img_df, batch_size=32):
   train_df, valid_df = skms.train_test_split(img_df, test_size=0.2,
                                              random_state=1, shuffle=True)
-  train_gen = images_generator(train_df, batch_size=batch_size)
-  steps_per_epoch = math.ceil(len(train_df) / batch_size)
-  valid_gen = images_generator(valid_df, batch_size=batch_size)
+  train_gen = images_generator(train_df, training=True, batch_size=batch_size)
+  # Augmentation factor
+  aug_factor = 2
+  steps_per_epoch = math.ceil(len(train_df) * aug_factor / batch_size)
+  valid_gen = images_generator(valid_df, training=False, batch_size=batch_size)
   valid_steps = math.ceil(len(valid_df) / batch_size)
 
   model = Sequential()
@@ -99,7 +113,7 @@ def driving_model(img_df, batch_size=32):
 def main(unused_argv):
   img_df = read_driving_log(os.path.expanduser(FLAGS.csv),
                             os.path.expanduser(FLAGS.root))
-  driving_model(img_df)
+  driving_model(img_df, batch_size=FLAGS.batch_size)
 
 
 if __name__ == "__main__":
